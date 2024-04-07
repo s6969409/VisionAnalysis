c1575e3da0435c69c12c7d230c842a81265defd1
Duke <s6969409@gmail.com>
2024-04-07 23:13:19 +0800

    Tool TepPerspectiveTransform add

---
+++ VisionAnalysis/UIs/Tools/Teps/TepPerspectiveTransform/TepPerspectiveTransform.cs
@@ -0,0 +1,96 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionAnalysis
+{
+    public class TepPerspectiveTransform : BaseToolEditParas
+    {
+        public TepPerspectiveTransform(ObservableRangeCollection<Nd> nodes) : base(nodes)
+        {
+            #region para value default...
+            Inputs["img1"] = new PInput() { value = new Mat() };
+            Inputs["img2"] = new PInput() { value = new Mat() };
+            Inputs["queryDescriptors"] = new PInput() { value = new Mat() };
+            Inputs["trainDescriptors"] = new PInput() { value = new Mat() };
+            Inputs["keypoints1"] = new PInput() { value = { } };
+            Inputs["keypoints2"] = new PInput() { value = { } };
+
+            Outputs["ProjectLink"] = new POutput() { value = new Mat() };
+            Outputs["Match"] = new POutput() { value = new Mat() };
+            #endregion
+        }
+        #region override BaseToolEditParas member
+        public override Action actionProcess => () =>
+        {
+            #region get input para
+            base.actionProcess();//read paras
+
+            Mat img1 = Inputs["img1"].value as Mat;
+            Mat img2 = Inputs["img2"].value as Mat;
+            Mat queryDescriptors = Inputs["queryDescriptors"].value as Mat;
+            Mat trainDescriptors = Inputs["trainDescriptors"].value as Mat;
+            IEnumerable<KeyPoint> keypoints1 = Inputs["keypoints1"].value as IEnumerable<KeyPoint>;
+            IEnumerable<KeyPoint> keypoints2 = Inputs["keypoints2"].value as IEnumerable<KeyPoint>;
+            #endregion
+
+            #region FLANN KnnMatch process
+            const int FLANN_INDEX_KDTREE = 1;
+            OpenCvSharp.Flann.KDTreeIndexParams indexParams = new OpenCvSharp.Flann.KDTreeIndexParams();
+            indexParams.SetInt("Algorithm", FLANN_INDEX_KDTREE);
+            indexParams.SetInt("Trees", 5);
+
+            OpenCvSharp.Flann.SearchParams searchParams = new OpenCvSharp.Flann.SearchParams();
+            searchParams.SetInt("Checks", 50);
+            FlannBasedMatcher flann = new FlannBasedMatcher(indexParams, searchParams);
+
+            DMatch[][] matches = flann.KnnMatch(queryDescriptors, trainDescriptors, k: 2);
+            #endregion
+
+            #region feature filt
+            List<DMatch> good = new List<DMatch>();
+            foreach (DMatch[] matchPair in matches)
+            {
+                foreach (DMatch match in matchPair)
+                {
+                    if (match.Distance < 0.7 * matchPair[1].Distance)
+                    {
+                        good.Add(match);
+                    }
+                }
+            }
+            #endregion
+
+            #region match process
+            int MIN_MATCH_COUNT = 0;
+            if (good.Count > MIN_MATCH_COUNT)
+            {
+                Point2d[] src_pts = good.Select(m => Point2d.FromPoint(keypoints1.ElementAt(m.QueryIdx).Pt.ToPoint())).ToArray();
+                Point2d[] dst_pts = good.Select(m => Point2d.FromPoint(keypoints2.ElementAt(m.TrainIdx).Pt.ToPoint())).ToArray();
+                Mat homographyMask = Cv2.FindHomography(src_pts, dst_pts, HomographyMethods.Ransac, 5.0);
+                int h = img1.Height;
+                int w = img1.Width;
+                Point2d[] pts = new Point2d[] { new Point(0, 0), new Point(0, h - 1), new Point(w - 1, h - 1), new Point(w - 1, 0) };
+                Point2d[] ptsPT = Cv2.PerspectiveTransform(pts, homographyMask);
+                Mat matMatch = img2.Clone();
+                Cv2.Polylines(matMatch, new List<Point[]> { ptsPT.Select(p => p.ToPoint()).ToArray() }, true, new Scalar(0, 0, 255), 3, LineTypes.AntiAlias);
+                Outputs["Match"].value = matMatch;
+            }
+            else
+            {
+                Outputs["Match"].value = null;
+            }
+            #endregion
+
+            #region Project
+            Mat result = new Mat();
+            Cv2.DrawMatches(img1, keypoints1, img2, keypoints2, good, result);
+            Outputs["ProjectLink"].value = result;
+            #endregion
+        };
+        #endregion
+    }
+}
