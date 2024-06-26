9d3aabfd304cd366052583794008364ee3e54e3c
Duke <s6969409@gmail.com>
2024-06-26 23:05:36 +0800

    Tool TepBoundingRect add

---
+++ VisionAnalysis/UIs/Tools/Teps/TepBoundingRect/TepBoundingRect.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionAnalysis
+{
+    public class TepBoundingRect : BaseToolEditParas
+    {
+        public TepBoundingRect(ObservableRangeCollection<Nd> nodes) : base(nodes)
+        {
+            #region para value default...
+            Inputs["InputImage"] = new PInput() { value = new Mat() };
+            Inputs["contours"] = new PInput();
+
+            Outputs["Output1"] = new POutput() { value = new Mat() };
+            Outputs["rects"] = new POutput() { value = new Mat() };
+            #endregion
+        }
+        #region override BaseToolEditParas member
+        public override Action actionProcess => () =>
+        {
+            #region get input para
+            base.actionProcess();//read paras
+
+            Mat source = Inputs["InputImage"].value as Mat;
+            Point[][] contours = Inputs["contours"].value as Point[][];
+            #endregion
+
+            //process...
+            Mat result = source.Clone();
+            Rect[] rects = contours.Select(contour => Cv2.BoundingRect(contour)).ToArray();
+            Array.ForEach(rects, rect => result.Rectangle(rect, Scalar.RandomColor()));
+            Outputs["Output1"].value = result;
+            Outputs["rects"].value = rects;
+        };
+        #endregion
+    }
+}
