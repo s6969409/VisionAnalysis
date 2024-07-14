4f78c1f49f87104eb090a9e5590270dfbca48ece
Duke <s6969409@gmail.com>
2024-07-14 08:35:23 +0800

    Tool TepAlgorithmB add

--- VisionAnalysis/UIs/Tools/Teps/TepAlgorithms/TepAlgorithms.cs
+++ VisionAnalysis/UIs/Tools/Teps/TepAlgorithms/TepAlgorithms.cs
@@ -167,4 +167,121 @@ namespace VisionAnalysis
             public double theta;
         }
     }
+
+    public class TepAlgorithmB : BaseToolEditParas
+    {
+        public TepAlgorithmB(ObservableRangeCollection<Nd> nodes) : base(nodes)
+        {
+            #region para value default...
+            Inputs["InputImage"] = new PInput() { value = new Mat() };
+            Inputs["contour"] = new PInput();
+            Inputs["contours"] = new PInput();
+            Inputs["range"] = new PInput() { value = 0.1 };
+            Inputs["index"] = new PInput() { value = 0 };
+
+            Outputs["Output1"] = new POutput() { value = new Mat() };
+            #endregion
+        }
+
+        #region override BaseToolEditParas member
+        public override Action actionProcess => () =>
+        {
+            #region get input para
+            base.actionProcess();//read paras
+
+            Mat source = Inputs["InputImage"].value as Mat;
+            Point[] contour = Inputs["contour"].value as Point[];
+            Point[][] contours = Inputs["contours"].value as Point[][];
+            double range = Convert.ToDouble(Inputs["range"].value);
+            int index = Convert.ToInt32(Inputs["index"].value);
+            #endregion
+
+            #region process...
+            Point[][] filtContours = contours.Where(c =>
+            {
+                double as1 = axisScale(c);
+                double as2 = axisScale(contour);
+
+                return as1 >= as2 * (1 - range) && as1 <= as2 * (1 + range);
+            }).Where(c =>
+            {
+                RotatedRect rRect = Cv2.MinAreaRect(c);
+                return rRect.Size.Width >= 1 && rRect.Size.Height >= 1;
+            }).ToArray();
+            RotatedRect rotatedTemplate = Cv2.MinAreaRect(contour);
+
+            var values = filtContours.Select(c => pts2Mat(c));
+
+            Outputs["Output1"].value = values.ElementAt(index);
+            updateUIImage(values.ElementAt(index));
+            Inputs["index"].value = index < values.Count() ? index + 1 : 0;
+            #endregion
+        };
+        #endregion
+        private Mat pts2Mat(Point[] contour)
+        {
+            Rect rect = Cv2.BoundingRect(contour);
+            Mat output = new Mat(rect.Size, MatType.CV_8UC1, Scalar.Black);
+            int ofsX = contour.Select(c => c.X).Min();
+            int ofsY = contour.Select(c => c.Y).Min();
+
+            Parallel.For(0, contour.Length, i =>
+            {
+                output.Set(contour[i].Y - ofsY, contour[i].X - ofsX, 255);
+            });
+
+            return output;
+        }
+        private class Contour
+        {
+            public int Index;
+            public Rect Rect;
+            public double Area;
+            public Point Pt;
+            public Size2f Size;
+            public double Angle;
+            public double Scale;
+            public double SizeScale;
+        }
+        private double axisScale(Point[] contour)
+        {
+            RotatedRect rRect = Cv2.MinAreaRect(contour);
+            double LongAxis = Math.Max(rRect.Size.Width, rRect.Size.Height);
+            double ShortAxis = Math.Min(rRect.Size.Width, rRect.Size.Height);
+            return LongAxis / ShortAxis;
+        }
+        private void drawAll(Mat mat, Point[][] contours, IEnumerable<Contour> ctMsg)
+        {
+            Array.ForEach(ctMsg.ToArray(), c => drawCtMsg(mat, c, contours[c.Index], Scalar.RandomColor()));
+        }
+        private void drawCtMsg(Mat mat, Contour ct, Point[] contour, Scalar scalar = default, int thickness = 1)
+        {
+            Cv2.DrawContours(mat, new Point[][] { contour }, -1, scalar, thickness);
+
+            Point pt1 = new Point(ct.Pt.X - 10 * (float)Math.Cos(ct.Angle * Math.PI / 180), ct.Pt.Y - 10 * (float)Math.Sin(ct.Angle * Math.PI / 180));
+            Point pt2 = new Point(ct.Pt.X + 10 * (float)Math.Cos(ct.Angle * Math.PI / 180), ct.Pt.Y + 10 * (float)Math.Sin(ct.Angle * Math.PI / 180));
+            Point pt3 = new Point(ct.Pt.X - 10 * (float)Math.Cos((ct.Angle + 90) * Math.PI / 180), ct.Pt.Y - 10 * (float)Math.Sin((ct.Angle + 90) * Math.PI / 180));
+            Point pt4 = new Point(ct.Pt.X + 10 * (float)Math.Cos((ct.Angle + 90) * Math.PI / 180), ct.Pt.Y + 10 * (float)Math.Sin((ct.Angle + 90) * Math.PI / 180));
+
+
+            Cv2.Line(mat, pt1, pt2, scalar, thickness);
+            Cv2.Line(mat, pt3, pt4, scalar, thickness);
+            Point anglePt = new Point(ct.Pt.X, ct.Pt.Y + 30);
+            Cv2.PutText(mat, $"Angle:{ct.Angle.ToString("0.000")}", anglePt, HersheyFonts.HersheyScriptSimplex, 1, scalar, thickness);
+            Point sPt = new Point(ct.Pt.X, ct.Pt.Y + 60);
+            Cv2.PutText(mat, $"Scale:{ct.Scale.ToString("0.000")}", sPt, HersheyFonts.HersheyScriptSimplex, 1, scalar, thickness);
+            Point ssPt = new Point(ct.Pt.X, ct.Pt.Y + 90);
+            Cv2.PutText(mat, $"SizeScale:{ct.SizeScale.ToString("0.000")}", ssPt, HersheyFonts.HersheyScriptSimplex, 1, scalar, thickness);
+        }
+        private class TT
+        {
+            public int Index;
+            public int nums;
+            public double xc;
+            public double yc;
+            public double xt;
+            public double yt;
+            public double theta;
+        }
+    }
 }
