1e2a57b6a82d76d3e37a9bdaf2862b620152ee8d
Duke <s6969409@gmail.com>
2024-07-14 22:01:48 +0800

    Tool TepHoughLinesP add

---
+++ VisionAnalysis/UIs/Tools/Teps/TepHough/TepHoughs.cs
@@ -0,0 +1,53 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisionAnalysis
+{
+    public class TepHoughLinesP : BaseToolEditParas
+    {
+        public TepHoughLinesP(ObservableRangeCollection<Nd> nodes) : base(nodes)
+        {
+            #region para value default...
+            Inputs["InputImage"] = new PInput() { value = new Mat() };
+            Inputs["rho"] = new PInput() { value = 1.0 };
+            Inputs["theta"] = new PInput() { value = 0.0175 };
+            Inputs["threshold"] = new PInput() { value = 100 };
+            Inputs["minLineLength"] = new PInput() { value = 0.0 };
+            Inputs["maxLineGap"] = new PInput() { value = 0.0 };
+
+            Outputs["Output1"] = new POutput() { value = new Mat() };
+            Outputs["Lines"] = new POutput();
+            #endregion
+        }
+        #region override BaseToolEditParas member
+        public override Action actionProcess => () =>
+        {
+            #region get input para
+            base.actionProcess();//read paras
+
+            Mat source = Inputs["InputImage"].value as Mat;
+            double rho = Convert.ToDouble(Inputs["rho"].value);
+            double theta = Convert.ToDouble(Inputs["theta"].value);
+            int threshold = Convert.ToInt32(Inputs["threshold"].value);
+            double minLineLength = Convert.ToDouble(Inputs["minLineLength"].value);
+            double maxLineGap = Convert.ToDouble(Inputs["maxLineGap"].value);
+            #endregion
+
+            //process...
+            LineSegmentPoint[] lines = Cv2.HoughLinesP(source, rho, theta, threshold, minLineLength, maxLineGap);
+            Mat result = new Mat(source.Size(), MatType.CV_8UC3, Scalar.Black);
+            foreach (LineSegmentPoint line in lines)
+            {
+                result.Line(line.P1, line.P2, Scalar.RandomColor());
+            }
+            Outputs["Output1"].value = result;
+            updateUIImage(result);
+            Outputs["Lines"].value = lines;
+        };
+        #endregion
+    }
+}
