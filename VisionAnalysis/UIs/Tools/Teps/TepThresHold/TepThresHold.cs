6939b71289054f4c40cabb7f4c01ff40713d1448
Duke <s6969409@gmail.com>
2025-01-18 14:05:36 +0800

    Tool TepAdaptiveThreshold add

--- VisionAnalysis/UIs/Tools/Teps/TepThresHold/TepThresHold.cs
+++ VisionAnalysis/UIs/Tools/Teps/TepThresHold/TepThresHold.cs
@@ -42,4 +42,40 @@ namespace VisionAnalysis
         #endregion
 
     }
+    
+    public class TepAdaptiveThreshold : BaseToolEditParas
+    {
+        public TepAdaptiveThreshold(ObservableRangeCollection<Nd> nodes) : base(nodes)
+        {
+            #region para value default...
+            Inputs["InputImage"] = new PInput() { value = new Mat() };
+            Inputs["maxValue"] = new PInput() { value = 255.0 };
+            Inputs["adaptiveMethod"] = new PInput() { value = AdaptiveThresholdTypes.GaussianC };
+            Inputs["thresholdType"] = new PInput() { value = ThresholdTypes.Binary };
+            Inputs["blockSize"] = new PInput() { value = 3 };
+            Inputs["c"] = new PInput() { value = 0.0 };
+
+            Outputs["Output1"] = new POutput() { value = new Mat() };
+            #endregion
+        }
+
+        #region override BaseToolEditParas member
+        public override Action actionProcess => () =>
+        {
+            base.actionProcess();//read paras
+
+            Cv2.AdaptiveThreshold(
+                (Mat)Inputs["InputImage"].value,
+                (Mat)Outputs["Output1"].value,
+                (double)Inputs["maxValue"].value,
+                TepHelper.getEnum<AdaptiveThresholdTypes>(Inputs["adaptiveMethod"].value),
+                TepHelper.getEnum<ThresholdTypes>(Inputs["thresholdType"].value),
+                (int)Inputs["blockSize"].value,
+                (double)Inputs["c"].value);
+            updateUIImage((Mat)Outputs["Output1"].value);
+            Outputs["Output1"].value = Outputs["Output1"].value;
+        };
+        #endregion
+
+    }
 }
